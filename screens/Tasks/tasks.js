243d5d588990de672ab47128204a5e1988486f44
lwfe <leonardolwfe@gmail.com>
2022-06-21 21:00:12 -0300

    fix: StatusBar

--- screens/Tasks/tasks.js
+++ screens/Tasks/tasks.js
@@ -97,7 +97,7 @@ export default function Tasks() {
       behavior={Platform.OS === "ios" ? "padding" : null}
       keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
     >
-      <StatusBar style="light" />
+      <StatusBar style="auto" />
       <List
         data={task}
         keyExtractor={(item) => item.toString()}
