476025560aaebef3eea5302c1213d088424101fb
lwfe <leonardolwfe@gmail.com>
2023-08-09 00:11:24 -0300

    chore: add path alias

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+  }
 }
