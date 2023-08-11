0cffa2983fe201a5d9b7bb36992b29a7a44ff298
lwfe <leonardolwfe@gmail.com>
2023-08-10 23:34:49 -0300

    test: add localAddTask useCase

---
+++ tests/data/useCases/localAddTask.spec.ts
@@ -0,0 +1,25 @@
+import { LocalAddTask } from '../../../src/data/useCases/localAddTask'
+import { LocalStorageAdapterSpy } from '../mocks/mockCache'
+
+const makeSut = (key: string) => {
+  const localStorage = new LocalStorageAdapterSpy()
+  const sut = new LocalAddTask(key, localStorage)
+  return {
+    sut,
+    localStorage
+  }
+}
+
+describe('LocalAddTask', () => {
+  it('should add a new task', async () => {
+    const { localStorage, sut } = makeSut('tasks')
+    const title = 'any_title'
+    await sut.add(title)
+    expect(localStorage.key).toBe('tasks')
+    expect(localStorage.value).toEqual({
+      id: expect.any(String),
+      title,
+      done: false
+    })
+  })
+})
